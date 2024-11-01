601932f4b76acc1fd14c99be9cdcd7466420b924
VenkateshAvvari23 <venkyavvari.99@gmail.com>
2024-11-01 18:37:57 +0530

    Venky

---
+++ src/com/tcs/Hello.java
@@ -0,0 +1,15 @@
+package com.tcs;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hii raaa");
+		
+		System.out.println("Venkyyy");
+		
+		System.out.println("Divya");
+	}
+	
+
+}
---
+++ src/com/tcs/Hi.java
@@ -0,0 +1,11 @@
+package com.tcs;
+
+public class Hi {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("Java ,Spring");
+	}
+
+}
